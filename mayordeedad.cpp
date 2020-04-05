067badfa076598131579b8548215d01c332223bb
jesusdelatorre30 <60176313+jesusdelatorre30@users.noreply.github.com>
2020-04-05 21:04:43 +0200

    Add files via upload

---
+++ mayordeedad.cpp
@@ -0,0 +1,14 @@
+#include <iostream> //Libreria
+using namespace std;
+main(){ //El cuerpo
+	int edad;
+	cout<<"ingrese su edad: ";cin>>edad;
+	if(edad>=18){
+		cout<<"Eres mayor de edad";
+	}
+	else if(edad<18){
+		cout<<"Eres menor de edad";
+	}	
+}
+
+
